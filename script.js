dc7b40106fffa335184a0533d8ff6472aa516941
Noah Howell <nhowell117@gmail.com>
2022-05-16 15:27:44 -0400

    Added delete button to book card

--- script.js
+++ script.js
@@ -8,10 +8,27 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-
+    // Gets all the values from input form
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read;
+    if (document.getElementById('read').checked) {
+        read = true;
+    }
+    else {
+        read = false;
+    }
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayLibrary(myLibrary);
+    closeForm();
 }
 
 function displayLibrary(myLibrary) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
     for (const book of myLibrary) {
         displayBook(book);
     }
@@ -20,6 +37,7 @@ function displayLibrary(myLibrary) {
 function displayBook(book) {
     let newBook = document.createElement('div');
     newBook.classList.add('card');
+    newBook.dataset.bookTitle = book.title;
 
     // Displays book's title
     let bookTitle = document.createElement('p');
@@ -39,19 +57,53 @@ function displayBook(book) {
     // Displays if you have read the book
     let bookRead = document.createElement('p');
     if (book.read === true) {
-        bookRead.textContent = "have read"
+        bookRead.textContent = "have read";
     }
     else {
-        bookRead.textContent = "have not read"
+        bookRead.textContent = "have not read";
     }
     newBook.appendChild(bookRead);
 
+    // Adds delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.addEventListener('click', deleteCard);
+    deleteBtn.textContent = "X";
+    deleteBtn.classList.add('delete-btn');
+    newBook.appendChild(deleteBtn);
+
+    // Appends final book to container
     container.appendChild(newBook);
 }
 
-const container = document.querySelector('.container');
+function deleteCard(e) {
+    const bookTitle = e.currentTarget.parentElement.dataset.bookTitle;
+    myLibrary.splice(myLibrary.findIndex(book => book.title === bookTitle), 1);
+    const collection = container.children;
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i].dataset.bookTitle === bookTitle) {
+            let deletedBook = collection[i];
+            container.removeChild(deletedBook);
+        }
+    }
+}
 
-myLibrary[0] = new Book("The Hobbit", "J.R.R. Tolkien", 310, true);
-myLibrary[1] = new Book("Moby Dick", "Herman Melville", 427, false);
+function changeReadStatus(e) {
+
+}
+
+function openForm() {
+    document.querySelector('.form-container').reset();
+    document.getElementById('addBookForm').style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById('addBookForm').style.display = "none";
+}
+
+const container = document.querySelector('.container');
+const addBookBtn = document.querySelector('#add-book-btn');
+addBookBtn.addEventListener('click', openForm);
+const submitBtn = document.querySelector('.submit-btn');
+submitBtn.addEventListener('click', addBookToLibrary);
 
 displayLibrary(myLibrary);
